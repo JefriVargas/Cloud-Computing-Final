06b49468d0a346d9667d1760a96b264dc166ca23
JefriVargas <143043024+JefriVargas@users.noreply.github.com>
2024-11-30 11:33:36 -0700

    Update handler.py

--- api_peliculas/handler.py
+++ api_peliculas/handler.py
@@ -3,12 +3,49 @@ import os
 import uuid
 import datetime
 import boto3
+import jwt
+from functools import wraps
+
+SECRET_KEY = os.environ.get('JWT_SECRET', 'mysecretkey')
+
+def validate_jwt(token):
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return decoded
+    except jwt.ExpiredSignatureError:
+        raise ValueError("El token ha expirado")
+    except jwt.InvalidTokenError:
+        raise ValueError("Token inválido")
+
+def jwt_required(func):
+    @wraps(func)
+    def wrapper(event, context, *args, **kwargs):
+        headers = event.get('headers', {})
+        auth_header = headers.get('Authorization')
+        if not auth_header or not auth_header.startswith("Bearer "):
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": "Se requiere un token válido en el encabezado Authorization"})
+            }
+        token = auth_header.split(" ")[1]  # Get the token part after "Bearer"
+        try:
+            decoded = validate_jwt(token)
+            event['user'] = decoded  # Add decoded JWT data to the event
+        except ValueError as e:
+            return {
+                "statusCode": 401,
+                "body": json.dumps({"error": str(e)})
+            }
+        return func(event, context, *args, **kwargs)
+    return wrapper
+
 
 # Configuración de DynamoDB
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE_PELICULAS'])
 
 # Listar todas las películas para un tenant específico
+@jwt_required
 def list_movies(event, context):
     tenant_id = event.get('queryStringParameters', {}).get('tenant_id')
     if not tenant_id:
@@ -33,6 +70,7 @@ def list_movies(event, context):
         }
 
 # Obtener detalles de una película específica para un tenant
+@jwt_required
 def get_movie_details(event, context):
     movie_id = event['pathParameters']['movie_id']
     tenant_id = event.get('queryStringParameters', {}).get('tenant_id')
@@ -67,6 +105,7 @@ def get_movie_details(event, context):
         }
 
 # Agregar una nueva película
+@jwt_required
 def add_movie(event, context):
     data = json.loads(event['body'])
     if 'titulo' not in data or 'genero' not in data or 'release_date' not in data or 'tenant_id' not in data:
